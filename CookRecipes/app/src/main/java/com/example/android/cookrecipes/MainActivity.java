a960388c9195d77cf04fffdeaf26766a272309eb
John Benavides <johnbsinfo92@gmail.com>
2016-04-29 12:36:49 -0600

    push proyecto Cook Recipes

---
+++ CookRecipes/app/src/main/java/com/example/android/cookrecipes/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.android.cookrecipes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    int asd;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        asd = 1;
+    }
+}
